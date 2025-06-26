a6faae9d5d1c33fb01f75677b46f8285573a4dc2
Nanupi <cedric.thenagels@gmail.com>
2025-06-26 09:49:26 +0200

    rm_Offer.cs

--- Offer.cs
+++ Offer.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eBayKleinanzeigenTracker
+class Offer
 {
-    class Offer
+    public long Id { get; }
+    public string Title { get; }
+    public string Description { get; }
+    public string Url { get; }
+    public string Thumbnail { get; }
+    public string Price { get; }
+    public DateTime? RemovedDate { get; set; }
+
+    public Offer(long id, string title, string description, string url, string thumbnail, string price, DateTime? removedDate = null)
     {
-        public long Id { get; }
-        public string Title { get; }
-        public string Description { get; }
-        public string Url { get; }
-        public string Thumbnail { get; }
-        public string Price { get; }
-
-        public Offer(long id, string title, string description, string url, string thumbnail, string price)
-        {
-            this.Id = id;
-            this.Title = title;
-            this.Description = description;
-            this.Url = url;
-            this.Thumbnail = thumbnail;
-            this.Price = price;
-        }
-
+        this.Id = id;
+        this.Title = title;
+        this.Description = description;
+        this.Url = url;
+        this.Thumbnail = thumbnail;
+        this.Price = price;
+        this.RemovedDate = removedDate;
     }
 }
